7f4836c3abd222e3f48a4f482b674a36e28c2228
RupeshF7R <rupeshkumarsharma283@gmail.com>
2023-12-28 08:08:55 +0530

    operation in js half done

---
+++ video-7/operation-in-js.js
@@ -0,0 +1,46 @@
+// ********************* Operations *********************
+
+let value = 9;
+let negValue = -value
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let str1 = "Rupesh"
+// let str2 = " sharma"
+// let strMix = str1 + str2;
+// console.log(strMix);
+
+// console.log("1" +3)
+// console.log(3 +"2")
+// console.log("3" +"2")
+// console.log("3" +"2" + true + false)
+// console.log("3" +"2" + 2 + 3)
+// console.log(3 + 2 + 2 + "3") // 👇🏻🤣
+//  3+2+2+"3" = 73 🤣🤣
+
+// console.log(("3" + "2") + (2 + 3))
+
+//  non sense bete 🤣🤣👇🏻👇🏻
+// console.log(-true)
+// console.log(-"")
+
+// assigenment operator
+
+let gameCounter = 100;
+++gameCounter;
+console.log([gameCounter]);
+
+// prefix means first starting things +-*/%
+// postfix means end closing things +-*/%
+
+
+
+ 
