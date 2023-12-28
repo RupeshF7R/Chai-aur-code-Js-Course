8570f926159dde02c49ba5664741b2cf76fa3dd8
RupeshF7R <rupeshkumarsharma283@gmail.com>
2023-12-28 07:38:05 +0530

    conversion on data type

---
+++ video-6/conversion-operation.js
@@ -0,0 +1,43 @@
+// const score = "Rupesh";
+// console.log([typeof score]);
+
+// let valueInNumber = Number(score);
+
+// console.log([typeof valueInNumber]);
+// console.log([valueInNumber]);
+
+//  when you convet a number they can convet easyly but
+// you print this you show the variable show number with string they are convert on the time
+
+// like number is 33ab ==> they show same on print time
+
+// let score = "Rupesh" ==> NaN
+// let score = "33ab" ==>  NaN
+// let score = true/false ==> 1/0
+// let score = null ==> NaN
+
+let isLoggedIn = null;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log([booleanIsLoggedIn]);
+
+//  when boolean conversion done 0 ==> false
+// 1 ==> true
+// "" ==> false
+// "ME" ==> true
+// null ==> false
+
+
+// string
+
+let someNUmber = null;
+
+let stringNumber = String(someNUmber);
+console.log({ stringNumber });
+console.log([typeof stringNumber]);
+
+
+// 33 ==> '33'
+// null ==> null
+
