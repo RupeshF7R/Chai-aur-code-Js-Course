fb212919738b9e97fb329722541926f2a6d965e8
RupeshF7R <rupeshkumarsharma283@gmail.com>
2023-12-29 10:27:31 +0530

    basic javascript info

---
+++ video-9/datatypes-summary.js
@@ -0,0 +1,6 @@
+console.log(["hello"]);
+
+/*
+JavaScript is a dynamically typed language. This means that variables do not need to be declared with a specific data type, and the data type of a variable can change at runtime. This can be both a blessing and a curse. On the one hand, it makes JavaScript code more flexible and easier to write. On the other hand, it can lead to errors if the programmer is not careful.
+*/
+
