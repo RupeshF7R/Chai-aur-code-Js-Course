e432f01e83f662a30ac3d9f9a107814ea3b8b9b8
RupeshF7R <rupeshkumarsharma283@gmail.com>
2023-12-29 09:38:53 +0530

    video 8 done comaprison-datatype

---
+++ video-8/Comparison-of-datatypes.js
@@ -0,0 +1,28 @@
+console.log("RupeshF7R");
+
+
+// != Not equal to
+// ==
+// < greater       > lessthen
+
+// console.log("2" < 3);
+// console.log(true > 0);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+console.log("2" === 3);
+
+
+//  basic conversion best conversion for the java-script
+
+// datatype conversion are avoid like null , undifiend and "String" Boolean
+
+
+
+//  when you use the === this means you chioce to choose the strict check for thr values 
