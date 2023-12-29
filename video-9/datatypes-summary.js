0af985394111d9ca4e6590ebd0a1268ae5fd052c
RupeshF7R <rupeshkumarsharma283@gmail.com>
2023-12-29 10:28:17 +0530

    primitve data types

--- video-9/datatypes-summary.js
+++ video-9/datatypes-summary.js
@@ -4,3 +4,31 @@ console.log(["hello"]);
 JavaScript is a dynamically typed language. This means that variables do not need to be declared with a specific data type, and the data type of a variable can change at runtime. This can be both a blessing and a curse. On the one hand, it makes JavaScript code more flexible and easier to write. On the other hand, it can lead to errors if the programmer is not careful.
 */
 
+
+// primitive data types
+
+// 7-8 types
+ 
+/*
+String
+number 
+boolean
+Null
+undfined
+symbol
+BigInt
+*/
+
+// const score = BigInt(100);
+// console.log([typeof score]);
+
+// Symbol defined
+const id = Symbol('22');
+// const anotherId = Symbol('123');
+// console.log([id === anotherId]);
+
+// bigInt 
+const bigNumber = 999999999888888888888444444n;
+
+// console.log([bigNumber])
+
