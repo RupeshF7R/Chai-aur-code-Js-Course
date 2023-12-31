d015a36cc446303083007bcce24d790f85ce2926
RupeshF7R <rupeshkumarsharma283@gmail.com>
2023-12-31 14:31:05 +0530

    use math.random( )

---
+++ video-12/math.js
@@ -0,0 +1,114 @@
+console.log(["Rupesh Sharma !"]);
+
+// ****************** MATHS *****************
+
+
+
+// console.log(Math);
+const mathNum = -99;
+
+// console.log([Math.abs(-99)]);
+
+// .abs change the negative valve to positive values
+// console.log([Math.round(4.5)]);
+// console.log([Math.ceil(4.5)]);
+
+// ceil use to define the higher value like u have 
+// 4.3 they give 5 direct because use ceil()
+// console.log([Math.floor(4.5)]);
+// ceil use to define the lower value like u have
+// 4.3 they give 4 direct because use floor()
+// round is use to choose the round off number
+//  like you have the 4.3 they give 4 and you have the
+// 4.5 value round() gives 5 value
+
+const sqrtP = 55;
+// console.log(Math.sqrt(55));
+
+
+// console.log(Math.min(6, 9 ,11, 3,3,2,4,1,0,9));
+// console.log(Math.max(6, 9 ,11, 3,3,2,4,1,0,9));
+
+
+
+
+console.log([`Using Maths.random`]);
+// console.log(Math.random());
+
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random() * 6) + 1);
+// console.log(Math.ceil(Math.random() * 6) + 1);
+
+// trick min max define
+
+const min = 1;
+const max = 6;
+
+//console.log(Math.ceil(Math.random() * (max - min + 0)) + min); // avoid zero
+//  this line use in ludo game for in this line of  code the zero are avoid 1 to 6
+// values are come in the output
+//console.log(Math.ceil(Math.random() * (min - max - 1)) + max); // avoid zero
+
+// formula
+
+/*
+abs: ƒ abs()
+acos: ƒ acos()
+acosh: ƒ acosh()
+asin: ƒ asin()
+asinh: ƒ asinh()
+atan: ƒ atan()
+atan2: ƒ atan2()
+atanh: ƒ atanh()
+cbrt: ƒ cbrt()
+ceil: ƒ ceil()
+clz32: ƒ clz32()
+cos: ƒ cos()
+cosh: ƒ cosh()
+exp: ƒ exp()
+expm1: ƒ expm1()
+floor: ƒ floor()
+fround: ƒ fround()
+hypot: ƒ hypot()
+imul: ƒ imul()
+log: ƒ log()
+log1p: ƒ log1p()
+log2: ƒ log2()
+log10: ƒ log10()
+max: ƒ max()
+min: ƒ min()
+pow: ƒ pow()
+random: ƒ random()
+round: ƒ round()
+sign: ƒ sign()
+sin: ƒ sin()
+sinh: ƒ sinh()
+sqrt: ƒ sqrt()
+tan: ƒ tan()
+tanh: ƒ tanh()
+trunc: ƒ trunc()
+
+*/
+
+/*
+constructor: ƒ Object()
+hasOwnProperty: ƒ hasOwnProperty()
+isPrototypeOf: ƒ isPrototypeOf()
+propertyIsEnumerable: ƒ propertyIsEnumerable()
+toLocaleString: ƒ toLocaleString()
+toString: ƒ toString()
+valueOf: ƒ valueOf()
+__defineGetter__: ƒ __defineGetter__()
+__defineSetter__: ƒ __defineSetter__()
+__lookupGetter__: ƒ __lookupGetter__()
+__lookupSetter__: ƒ __lookupSetter__()
+__proto__: ƒ __proto__()
+set __proto__: ƒ __proto__()
+
+*/
+
+
+const minValue = 5;
+const maxValue = 9;
+
+console.log(Math.ceil(Math.random()*10));
